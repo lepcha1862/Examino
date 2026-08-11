74046b69cb8b9f18f626ea59c16d495b8885688b
lepcha1862 <gchalep07@gmail.com>
2026-08-11 17:35:45 +0600

    video added

--- examino/src/app/login_components/auth/AuthLayout.js
+++ examino/src/app/login_components/auth/AuthLayout.js
@@ -1,20 +1,27 @@
 export default function AuthLayout({ children }) {
   return (
     <main className="relative min-h-screen w-full overflow-hidden bg-slate-950">
-      {/* Background Image */}
+      {/* Background Video */}
       <div className="absolute inset-0">
-        <img
-          src="/login-bg.png"
-          alt=""
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
           className="
             h-full
             w-full
             object-cover
             object-center
           "
-        />
+        >
+          <source
+            src="https://res.cloudinary.com/ozxfkvzd/video/upload/v1785814802/10720792-hd_1920_1080_30fps_rk3krx.mp4"
+            type="video/mp4"
+          />
+        </video>
 
-        {/* Dark overlay */}
+        {/* Dark Overlay */}
         <div className="absolute inset-0 bg-slate-950/30" />
       </div>
 
@@ -25,14 +32,11 @@ export default function AuthLayout({ children }) {
           z-10
           min-h-screen
           w-full
-
           flex
           items-center
           justify-center
-
           px-5
           py-10
-
           lg:justify-end
           lg:px-[7%]
           xl:px-[8%]
